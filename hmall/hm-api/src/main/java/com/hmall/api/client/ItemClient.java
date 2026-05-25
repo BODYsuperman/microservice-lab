af0b0609c8dd7327336ea70712cd256a85661fba
alex <962213127@qq.com>
2026-05-25 15:29:52 +0800

    feat: add pay module

--- hmall/hm-api/src/main/java/com/hmall/api/client/ItemClient.java
+++ hmall/hm-api/src/main/java/com/hmall/api/client/ItemClient.java
@@ -19,6 +19,6 @@ public interface ItemClient {
     @GetMapping("/items")
     List<ItemDTO> queryItemByIds(@RequestParam("ids") Collection<Long> ids);
 
-    @PutMapping("items//stock/deduct")
+    @PutMapping("/items//stock/deduct")
     void deductStock(@RequestBody List<OrderDetailDTO> items);
 }
