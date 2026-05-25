db02637f9e1ac75cde36e2ebf3513369def3abdc
alex <962213127@qq.com>
2026-05-25 11:27:02 +0800

    add openfeign connection pool

--- hmall/cart-service/src/main/java/com/hmall/cart/CartApplication.java
+++ hmall/cart-service/src/main/java/com/hmall/cart/CartApplication.java
@@ -1,18 +1,20 @@
 package com.hmall.cart;
 
 //import com.hmall.api.config.FeignLogLevelConfig;
+import com.hmall.api.config.DefaultFeignConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 //@EnableFeignClients(basePackages = "com.hmall.api.client", defaultConfiguration = FeignLogLevelConfig.class)
 @MapperScan("com.hmall.cart.mapper")
 @SpringBootApplication
-@EnableFeignClients
+@EnableFeignClients(basePackages = "com.hmall.api.client" , defaultConfiguration = DefaultFeignConfig.class)
 public class CartApplication {
     public static void main(String[] args) {
         SpringApplication.run(CartApplication.class, args);
