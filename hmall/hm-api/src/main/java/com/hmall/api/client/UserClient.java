b07faea9b1593804afdaa39c1c6d6dcf353cec75
alex <962213127@qq.com>
2026-05-26 10:02:33 +0800

    add gateway module

--- hmall/hm-api/src/main/java/com/hmall/api/client/UserClient.java
+++ hmall/hm-api/src/main/java/com/hmall/api/client/UserClient.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient(value = "user-service", configuration = DefaultFeignConfig.class)
 public interface UserClient {
 
-    @PutMapping("users//money/deduct")
+    @PutMapping("/users/money/deduct")
     void deductMoney(@RequestParam("pw") String pw, @RequestParam("amount") Integer amount);
 }
---
+++ hmall/hm-common/src/main/java/com/hmall/common/config/MvcConfig.java
@@ -0,0 +1,16 @@
+package com.hmall.common.config;
+
+import com.hmall.common.interceptor.UserInfoInterceptor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@ConditionalOnClass(WebMvcConfigurer.class)//由于网关没有SpringMVC且不需要这个配置，所以做一个条件判断
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new UserInfoInterceptor());
+    }
+}
